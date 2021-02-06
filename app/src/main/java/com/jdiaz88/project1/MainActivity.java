fcd6cda8a8556183a340dddde7ef05cb07e2ea00
Jacob Diaz <hello@jacobdiaz.dev>
2021-02-06 00:08:23 -0600

    Completed All Tasks (need to style & implement landscape)

--- app/src/main/java/com/jdiaz88/project1/MainActivity.java
+++ app/src/main/java/com/jdiaz88/project1/MainActivity.java
@@ -1,22 +1,28 @@
 // Jacob Diaz
 // Jdiaz88@uic.edu
 // Project 1
+// How to put contact into contacts app https://developer.android.com/training/contacts-provider/modify-data
 package com.jdiaz88.project1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+//    TODO Create Lanscape Orientations for screens!!!!
+
     private static final int requestCode = 99;
     private TextView centerText;
     private TextView welcomeText;
     private boolean isValidLegalName;
+    private String legalName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +45,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void handleButtonTwoClicked(View view) {
+        if(isValidLegalName){
+            Intent contactsIntent = new Intent(ContactsContract.Intents.Insert.ACTION);
+            contactsIntent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
 
+            contactsIntent.putExtra(ContactsContract.Intents.Insert.NAME, legalName);
+            startActivity(contactsIntent);
+        }
+        else{
+            Toast.makeText(this, "Please enter valid name by clicking Button One", Toast.LENGTH_SHORT).show();
+        }
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent i) {
@@ -51,8 +66,8 @@ public class MainActivity extends AppCompatActivity {
             if(resultCode == RESULT_OK){
                 // Get our data
                 String result = i.getStringExtra("legalName");
-                Log.d("Got Name: ", " "+result );
-                welcomeText.setText("Welcome "+result);
+                legalName = result;
+                welcomeText.setText("Welcome "+legalName);
                 centerText.setText("Name Validated!");
                 isValidLegalName = true;
             }
